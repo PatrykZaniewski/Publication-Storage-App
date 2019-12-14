2502f27cef37aa077401c58a218e341eb3fb8e70
Unknown <kajgore@vp.pl>
2019-12-14 17:52:14 +0100

    Javafx added

---
+++ desktop/src/Controller.java
@@ -0,0 +1,14 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    public Button loginButton;
+    public TextField loginField;
+    public PasswordField passwordField;
+
+
+}
